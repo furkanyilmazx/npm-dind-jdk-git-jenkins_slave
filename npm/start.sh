15dce4e10310d796d2f969b5a8cbde556c48a0c8
fyx <furkanyilmazx@gmail.com>
2019-02-06 23:33:00 +0300

    Add buuld to docker image

--- npm/start.sh
+++ npm/start.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-
-nohup java -jar agent.jar -jnlpUrl http://192.168.0.34:8080/computer/npm/slave-agent.jnlp -secret 714536e0af8982b5514f9ac8bf9ecfa8756ea1310bb55f13f8deaa2dbf398930 -workDir "/home/fyx" &
-
-
-dockerd
+service docker start && sleep 5 && echo { \"storage-driver\" : \"vfs\" } >>  /etc/docker/daemon.json && service docker restart &&java -jar agent.jar -jnlpUrl http://192.168.0.34:8080/computer/npm/slave-agent.jnlp -secret 714536e0af8982b5514f9ac8bf9ecfa8756ea1310bb55f13f8deaa2dbf398930 -workDir "/home/fyx"
